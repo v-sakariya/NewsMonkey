9a3bc06779cc195b7ce7cdc4e9fdb1a7b3b86731
Vivek <sakariyavivek12@gmail.com>
2023-07-18 12:56:52 -0300

    Infinite Scrolling Enabled

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,42 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:1}}>
-            {source}
-            <span class="visually-hidden">unread messages</span>
-          </span>
-          <img
-            src={
-              imageUrl
-                ? imageUrl
-                : "https://www.livemint.com/lm-img/img/2023/04/15/600x338/2-0-1244114454-iStock-839214100-0_1679615828655_1681579000066_1681579000066.jpg"
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p class="card-text">
-              <small class="text-body-secondary">
-                By {!author ? "Unknown" : author} on{" "}
-                {new Date(date).toUTCString()}
-              </small>
-            </p>
-            <a href={newsUrl} className="btn btn-sm btn-primary">
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
+
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default NewsItem;
+export default NewsItem
